9d094e423b9bbedf60855c09643c8468e65a2121
Hai Nguyen <nguyentrunghai2106@gmail.com>
2023-09-16 14:02:45 +0700

    20230916 - Hoan thanh Shopping Admin

--- src/main/java/com/donations/admin/GoogleCloudStorageConfig.java
+++ src/main/java/com/donations/admin/GoogleCloudStorageConfig.java
@@ -1,11 +1,12 @@
 package com.donations.admin;
 
 import java.io.ByteArrayInputStream;
+import org.springframework.core.io.Resource;
 import java.io.IOException;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.core.io.FileSystemResource;
 import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
